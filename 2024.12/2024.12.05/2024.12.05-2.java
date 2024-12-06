15d3a7fdee8ed62dc73bb78c5f026fec247f04d7
woong <woong7361@gmail.com>
2024-12-06 11:54:46 +0900

    2024.12.06

--- 2024.12/2024.12.05/2024.12.05-2.java
+++ 2024.12/2024.12.05/2024.12.05-2.java
@@ -1,67 +1,66 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/340198
-
-import java.util.*;
-import java.util.stream.Collectors;
+// https://school.programmers.co.kr/learn/courses/30/lessons/258712
 
 
-class Solutions {
-    public int solution(int[] mats, String[][] park) {
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
         int answer = 0;
 
-        Arrays.sort(mats);
-        int maxLength = mats[mats.length - 1];
+        // 선물을 누가 많이 받을지 예측
+        // 더 많이 선물 준사람이 선물 하나 받음 - 같다면?
+        // A == B or A = B = 0 -> 선물지수가 큰사람이 작은사람에게 선물을 하나 받는다.
+        // 선물지수 = 선물은 준 수 - 선물을 받은 수
+        // 선물지수 또한 같다면 선물을 주고받지 않는다.
+        // 선물을 가장 많이 받는 친구가 받을 선물의 개수는?
 
-        // 정사각형 모양 돗자리
-        // 깔수있는 가장 큰 돗자리
 
-        // park 확인 -> 이미 자리있다면 넘어가기
-        // 자리 없다면? check 해야지 어떻게?  그냥? full 연산?
-        for (int row = 0; row < park.length; row++) {
-            for (int col = 0; col < park[0].length; col++) {
-                if (park[row][col].equals("-1")) {
+        // 누가 선물을 더 많이 주었나 판별해야함
+            // 선물 map 작성 ex. muzi frodo : 3
+        HashMap<String, Integer> giftCountMap = new HashMap<>();
+        HashMap<String, Integer> giftScoreMap = new HashMap<>();
+        for (String gift : gifts) {
+            giftCountMap.put(gift, giftCountMap.getOrDefault(gift, 0) + 1);
 
-                    //check max mats
-                    // 1일때 2일때 3일때 ... maxLength-1 일때까지 한줄씩 확인하기
-                    // r,c   r+1,c   r+2,c
-                    // r,c+1 r+1,c+1 r+2,c+1
-                    // r,c+2 r+1,c+2 r+2,c+2
-                    for (int i = 0; i < maxLength; i++) {
-                        boolean flag = false;
-                        // row
-                        for (int j = 0; j <= i; j++) {
-                            if (!(row + i < park.length && col + j < park[0].length && park[row + i][col + j].equals("-1"))) {
-                                flag = true;
-                                break;
-                            }
+            // 선물 지수를 판별해야함
+                // 선물 map 작성하면서 받으면 -1, 주면 +1
+            String[] split = gift.split(" ");
+            String sender = split[0];
+            String receiver = split[1];
 
-                            if (!(row + j < park.length && col + i < park[0].length && park[row + j][col + i].equals("-1"))) {
-                                flag = true;
-                                break;
-                            }
-                        }
+            giftScoreMap.put(sender, giftScoreMap.getOrDefault(sender, 0) + 1);
+            giftScoreMap.put(receiver, giftScoreMap.getOrDefault(receiver, 0) - 1);
+        }
 
-                        if (flag) {
-                            break;
-                        }
+        // 각 friends가 다른 friends를 확인하면서 선물을 줘야하는지 확인 -> 받는건 중복
+        HashMap<String, Integer> resultMap = new HashMap<>();
+        for (String friend : friends) {
+            for (String targetFriend : friends) {
+                if (friend.equals(targetFriend)) {
+                    continue;
+                }
+                // 선물을 줘야하는지 확인하고, 주어야한다면 resultMap +1
 
-                        answer = Math.max(answer, i + 1);
+                if (giftCountMap.getOrDefault(friend + " " + targetFriend, 0) > giftCountMap.getOrDefault(targetFriend + " " + friend, 0)) {
+                    // 내가 준게 더 많다면 받아야지
+                    continue;
+                } else if (giftCountMap.getOrDefault(friend + " " + targetFriend, 0) < giftCountMap.getOrDefault(targetFriend + " " + friend, 0)) {
+                    // 내가 준게 더 적다면 줘야지
+                    resultMap.put(targetFriend, resultMap.getOrDefault(targetFriend, 0) + 1);
+                } else {
+                    // 준게 같거나 없다면
+                    if (giftScoreMap.getOrDefault(friend, 0) < giftScoreMap.getOrDefault(targetFriend, 0)) {
+                        // score가 내가 작다면 줘야지
+                        resultMap.put(targetFriend, resultMap.getOrDefault(targetFriend, 0) + 1);
                     }
-
                 }
             }
         }
 
-        // 오름차순 정렬이지
-        // 1 3 5 , a=4
-        int result = -1;
-        for (int mat : mats) {
-            if (mat <= answer) {
-                result = mat;
-            } else {
-                break;
-            }
+        for (Integer value : resultMap.values()) {
+            answer = Math.max(answer, value);
         }
 
-        return result;
+        return answer;
     }
 }
