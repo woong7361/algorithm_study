ee7069db6a313bb5b8faf2783982cf8eddb0958b
woong <woong7361@gmail.com>
2025-01-21 11:24:41 +0900

    2025.01.24

--- 2025.01/2025.01.16/2025.01.16-1.java
+++ 2025.01/2025.01.16/2025.01.16-1.java
@@ -1,58 +1,78 @@
-// https://leetcode.com/problems/longest-palindromic-substring/solutions/5276071/video-using-two-pointers-python-javascript-java-c/
+package org.example.solutions;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/176962
+
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
-    public String longestPalindrome(String s) {
-        if (s.length() <= 0) {
-            return "";
-        }
+    public String[] solution(String[][] plans) {
+        String[] answer = {};
 
-        String result = s.substring(0, 1);
+        //왼쪽이 더 크면 양수 -> 오름차순(ASC) default
+        List<String[]> sortedPlans = Arrays.stream(plans)
+                .sorted((strings, temp) -> {
+                    String[] t1 = strings[1].split(":");
+                    String[] t2 = temp[1].split(":");
+                    if (Integer.parseInt(t1[0]) > Integer.parseInt(t2[0])) {
+                        return 1;
+                    } else if (Integer.parseInt(t1[0]) < Integer.parseInt(t2[0])) {
+                        return -1;
+                    } else {
+                        if (Integer.parseInt(t1[1]) > Integer.parseInt(t2[1])) {
+                            return 1;
+                        } else {
+                            return -1;
+                        }
+                    }
+                })
+                .collect(Collectors.toList());
 
-        // 홀수일 때 -> i+1, i-1 같아야함
-        for (int i = 1; i < s.length()-1; i++) {
+        Deque<String[]> pauses = new ArrayDeque<>();
+        ArrayList<String> result = new ArrayList<>();
 
-            int left = i - 1;
-            int right = i + 1;
-            while (left >= 0 && right < s.length()) {
-                if (s.charAt(left) != s.charAt(right)) {
-                    break;
+        for (int i = 0; i < sortedPlans.size(); i++) {
+            String[] plan = sortedPlans.get(i);
+
+            // 마지막 원소면 그냥 추가
+            if (i == sortedPlans.size() - 1) {
+                result.add(plan[0]);
+                break;
+            }
+
+            String[] nextPlan = sortedPlans.get(i+1);
+            int timeInterval = getTimeInterval(plan[1], nextPlan[1]);
+            pauses.push(plan);
+
+            // time Interval 쓰기
+            while (timeInterval > 0 && !pauses.isEmpty()) {
+                String[] pop = pauses.pop();
+
+                if (timeInterval >= Integer.parseInt(pop[2])) {
+                    timeInterval = timeInterval - Integer.parseInt(pop[2]);
+                    result.add(pop[0]);
+                } else {
+                    pauses.push(new String[]{pop[0], pop[1], String.valueOf(Integer.parseInt(pop[2]) - timeInterval)});
+                    timeInterval = 0;
                 }
-                left--;
-                right++;
-            }
-
-            String temp = s.substring(left+1, right);
-
-            if (result.length() < temp.length()) {
-                result = temp;
             }
         }
 
-        // 짝수일 때 우선 i, i+1 같아야하고 -> i-1 i+2 같아야함
-        for (int i = 0; i < s.length()-1; i++) {
-            if (s.charAt(i) != s.charAt(i + 1)) {
-                continue;
-            }
-
-            int left = i - 1;
-            int right = i + 2;
-            while (left >= 0 && right < s.length()) {
-                if (s.charAt(left) != s.charAt(right)) {
-                    break;
-                }
-                left--;
-                right++;
-            }
-
-            String temp = s.substring(left+1, right);
-
-            if (result.length() < temp.length()) {
-                result = temp;
-            }
+        while (!pauses.isEmpty()) {
+            result.add(pauses.pop()[0]);
         }
 
-        return result;
+        return result.toArray(new String[0]);
+    }
+
+    private int getTimeInterval(String t1, String t2){
+        String[] split1 = t1.split(":");
+        int t1Time = Integer.parseInt(split1[0]) * 60 + Integer.parseInt(split1[1]);
+
+        String[] split2 = t2.split(":");
+        int t2Time = Integer.parseInt(split2[0]) * 60 + Integer.parseInt(split2[1]);
+
+        return t2Time - t1Time;
     }
 }
--- javamain/src/main/java/org/example/solutions/Solution.java
+++ javamain/src/main/java/org/example/solutions/Solution.java
@@ -1,36 +1,78 @@
 package org.example.solutions;
 
-// https://school.programmers.co.kr/learn/courses/30/lessons/176963
+// https://school.programmers.co.kr/learn/courses/30/lessons/176962
 
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
-    public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[photo.length];
+    public String[] solution(String[][] plans) {
+        String[] answer = {};
 
-        // 추억점수
-        // 사진속에 나오는 인물의 그리움 점수 합산
-        // 사람이 다수일때는 각 사람의 그리움 점수 합산
+        //왼쪽이 더 크면 양수 -> 오름차순(ASC) default
+        List<String[]> sortedPlans = Arrays.stream(plans)
+                .sorted((strings, temp) -> {
+                    String[] t1 = strings[1].split(":");
+                    String[] t2 = temp[1].split(":");
+                    if (Integer.parseInt(t1[0]) > Integer.parseInt(t2[0])) {
+                        return 1;
+                    } else if (Integer.parseInt(t1[0]) < Integer.parseInt(t2[0])) {
+                        return -1;
+                    } else {
+                        if (Integer.parseInt(t1[1]) > Integer.parseInt(t2[1])) {
+                            return 1;
+                        } else {
+                            return -1;
+                        }
+                    }
+                })
+                .collect(Collectors.toList());
 
-        // name 그리워하는 사람들
-        // 각 사람별 그리움점수 정수 배열 yearning
+        Deque<String[]> pauses = new ArrayDeque<>();
+        ArrayList<String> result = new ArrayList<>();
 
-        // name -> yearning map 생성 - 그리움 점수 map
-        // photo iter
-        //  photo 내부의 사람들이 yearning map에 있다면 점수 추가, 없다면 넘어가기
+        for (int i = 0; i < sortedPlans.size(); i++) {
+            String[] plan = sortedPlans.get(i);
 
-        HashMap<String, Integer> yearningMap = new HashMap<>();
-        for (int i = 0; i < name.length; i++) {
-            yearningMap.put(name[i], yearning[i]);
-        }
+            // 마지막 원소면 그냥 추가
+            if (i == sortedPlans.size() - 1) {
+                result.add(plan[0]);
+                break;
+            }
 
-        for (int i = 0; i < photo.length; i++) {
-            for (String personInPhoto : photo[i]) {
-                answer[i] += yearningMap.getOrDefault(personInPhoto, 0);
+            String[] nextPlan = sortedPlans.get(i+1);
+            int timeInterval = getTimeInterval(plan[1], nextPlan[1]);
+            pauses.push(plan);
+
+            // time Interval 쓰기
+            while (timeInterval > 0 && !pauses.isEmpty()) {
+                String[] pop = pauses.pop();
+
+                if (timeInterval >= Integer.parseInt(pop[2])) {
+                    timeInterval = timeInterval - Integer.parseInt(pop[2]);
+                    result.add(pop[0]);
+                } else {
+                    pauses.push(new String[]{pop[0], pop[1], String.valueOf(Integer.parseInt(pop[2]) - timeInterval)});
+                    timeInterval = 0;
+                }
             }
         }
 
-        return answer;
+        while (!pauses.isEmpty()) {
+            result.add(pauses.pop()[0]);
+        }
+
+        return result.toArray(new String[0]);
+    }
+
+    private int getTimeInterval(String t1, String t2){
+        String[] split1 = t1.split(":");
+        int t1Time = Integer.parseInt(split1[0]) * 60 + Integer.parseInt(split1[1]);
+
+        String[] split2 = t2.split(":");
+        int t2Time = Integer.parseInt(split2[0]) * 60 + Integer.parseInt(split2[1]);
+
+        return t2Time - t1Time;
     }
 }
