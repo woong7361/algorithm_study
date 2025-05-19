cd9c2ef12b9a3e1c2b995ccd461ba752112745ab
woong <woong7361@gmail.com>
2025-05-19 22:01:22 +0900

    2025.05.19

--- javamain/src/main/java/org/example/solutions/Solution.java
+++ javamain/src/main/java/org/example/solutions/Solution.java
@@ -8,8 +8,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    List<String> resultList = new ArrayList<>();
-
+    static List<HashSet<Integer>> resultList = new ArrayList<>();
     public int solution(String[][] relation) {
         resultList = new ArrayList<>();
         // 후보키의 최대 개수를 구하시오
@@ -24,17 +23,16 @@ class Solution {
         // 유일성 검사 -> SET 으로 해결
         // 최소성 검사 -> LIST CONTAIN 연산으로 해결
         for (int i = 1; i < relation[0].length + 1; i++) {
-            permutation(relation, new ArrayList<>(), i);
+            permutation(relation, new HashSet<>(), i);
         }
 
         return resultList.size();
     }
 
     // 자기 이후 번호(컬럼)만 재귀
-    private void permutation(String[][] relation, List<Integer> key, int size) {
+    private void permutation(String[][] relation, HashSet<Integer> key, int size) {
         // 원소의 개수가 작은 순서대로 실행
         if (key.size() == size) {
-            System.out.println("key = " + getKeyString(key));
             // 유일성 체크
             if (uniqueTest(key)) {
                 return;
@@ -42,30 +40,35 @@ class Solution {
 
             // 최소성 체크
             if (minimalityTest(relation, key)) {
-                resultList.add(getKeyString(key));
+                resultList.add(new HashSet<>(key));
             }
 
             return;
         }
 
         // 재귀
-        for (int column = key.isEmpty() ? 0 : key.get(key.size() - 1) + 1; column < relation[0].length; column++) {
+        OptionalInt OptionalMax = key.stream()
+                .mapToInt(it -> Integer.valueOf(it))
+                .max();
+
+        for (int column = OptionalMax.isEmpty() ? 0 : OptionalMax.getAsInt() + 1; column < relation[0].length; column++) {
             key.add(column);
             permutation(relation, key, size);
             key.remove(Integer.valueOf(column));
         }
     }
 
-    private boolean uniqueTest(List<Integer> key) {
+    private boolean uniqueTest(HashSet<Integer> key) {
         return resultList.stream()
-                .anyMatch(candidateKey -> getKeyString(key).contains(candidateKey));
+                .anyMatch(candidateKey -> key.containsAll(candidateKey));
     }
 
-    private boolean minimalityTest(String[][] relation, List<Integer> key) {
+    private boolean minimalityTest(String[][] relation, HashSet<Integer> key) {
         HashSet<String> set = Arrays.stream(relation)
                 .map(row -> {
                     StringBuilder value = new StringBuilder();
-                    key.stream()
+                    key
+                            .stream()
                             .forEach(column -> value.append(row[column]));
 
                     return value.toString();
@@ -75,7 +78,7 @@ class Solution {
         return set.size() == relation.length;
     }
 
-    private String getKeyString(List<Integer> key) {
+    private String getKeyString(HashSet<Integer> key) {
         StringBuilder t = new StringBuilder();
 
         key.stream()
