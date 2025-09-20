b4e05dc664ee5da78dad8dfed68c1a50e84b8115
woong <woong7361@gmail.com>
2025-09-20 19:49:38 +0900

    2025.09.20

--- javamain/src/main/java/org/example/Main.java
+++ javamain/src/main/java/org/example/Main.java
@@ -1,12 +1,11 @@
 package org.example;
 
-// https://www.acmicpc.net/problem/1052
-// 38 start
+// https://www.acmicpc.net/problem/16236
+// 30 start
 
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static double maxResult = -1;
@@ -15,77 +14,80 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // N개의 물병
-        // 모든 물병에는 물이 1L씩 들어있다.
-        // 한번에 K개의 물병을 옮길 수 있다.
-        // 물병의 물을 재분배해, k개를 넘지 않는 비어있지 않은 물병을 만드려한다.
+        // N*N 크기의 공간
+        // 물고기 M마리, 아기상어
+        // 아기상어의 크기 2, 1초에 1칸 이동 (상하좌우)
+        // 자신의 크기보다 큰 물고기칸 못지나간다. 자기와 같은 크기는 지나갈 수 있지만 못먹는다. 자신보다 작은 크기는 먹을 수 있다.
+        // 더이상 못먹으면 엄마에게 도움을 요청한다.
 
-        // 물의 재분배 법칙
-        // 같은 양의 물병 2개를 고른다. 그다음 한개의 물병에 다른 한쪽에 있는 물을 모두 붇는다.
-        // 이때문에 K개를 넘지 않는 비어있지 않은 물병을 만드는 것이 불가능할 수 도 있다.
-        // 따라서 상점에서 물병을 살 수 있다. (1L)
+        // 먹을 수 있는 물고기가 있을 때 가장 가까운 고기를 먹으러간다.
+        // 거리가 같다면 가장 위, 왼쪽에 있는 물고기를 우선한다.
+        // 자신의 크기와 같은 수의 물고기를 먹을 때 마다 크기가 1 증가한다.
 
-        // 상점에서 사야하는 물병의 최솟값을 구하여라
-        // 물병에 물을 무한대로 부을 수 있다.
-
-        // 1 <= N < 10^7 , 1 <= K <= 1000
-
-        // 1 1 1 1 1 1 1 1 1 1 1 1 1
-        // 2 2 2 2 2 2 1
-        // 4 4 4 1
-        // 8 4 1
-        // 8 4 2 +1
-        // 8 4 4 +2
-        // 8 8
 
         // 문제 설계
-        // N/2 -> 2의 개수  N%2 -> 1의 개수
-        // 반복
-        // int[] 배열에 담아둔다.
-        // sum을 반복하면서 상점에서 살 물건을 구한다.
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        int[] waters = new int[100];
-
-        int liter = 1;
-        while (N > 0) {
-            waters[liter] = N / 2;
-            waters[liter - 1] = N % 2;
-            N = N / 2;
-            liter++;
+        // bfs 탐색
+        // PriorityQueue에 같은 depth를 모두 넣고 정렬 후 확인 하는 방식으로 구현
+        int N = Integer.parseInt(br.readLine());
+        int[][] ocean = new int[N][N];
+        int[] sharkPoint = new int[2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                ocean[i][j] = Integer.parseInt(st.nextToken());
+                if (ocean[i][j] == 9) {
+                    sharkPoint[0] = i;
+                    sharkPoint[i] = j;
+                }
+            }
         }
 
-        int buyCount = 0;
-        for (int i = 0; i < 99; i++) {
-            System.out.println(Arrays.toString(waters));
-            if (waters[i] > 1) {
-                waters[i + 1] += waters[i] / 2;
-                waters[i] = waters[i] % 2;
-            }
+        PriorityQueue<int[]> currentGo = new PriorityQueue<>((t1, t2) -> {
+            // 위쪽 우선 i 가 작은게 앞으로
+            // 왼쪽 우선 j가 작은게 앞으로
+            if (t1[0] != t2[0]) return t1[0] - t2[0];
+            else return t1[1] - t2[1];
+        });
+        PriorityQueue<int[]> nextGo = new PriorityQueue<>((t1, t2) -> {
+            // 위쪽 우선 i 가 작은게 앞으로
+            // 왼쪽 우선 j가 작은게 앞으로
+            if (t1[0] != t2[0]) return t1[0] - t2[0];
+            else return t1[1] - t2[1];
+        });
 
-            if (Arrays.stream(waters).sum() <= K) {
-                break;
-            }
+        int time = 0;
 
-            if (waters[i] == 1) {
-                buyCount += (1 << i);
-                waters[i] = 0;
-                waters[i + 1] += 1;
-            }
+        // 못먹을때 까지
+        boolean isEat = true;
+        while (isEat) {
+            isEat = false;
 
+            // current 탐색
+            currentGo.add(sharkPoint);
+            while (!currentGo.isEmpty()) {
+                int[] poll = currentGo.poll();
+                //
+
+                if (currentGo.isEmpty()) {
+                    currentGo = nextGo;
+                    nextGo =
+                }
+            }
         }
 
-        if (Arrays.stream(waters).sum() <= K) {
-            System.out.println(buyCount);
-        } else {
-            System.out.println(-1);
-        }
 
         br.close();
     }
+
+    public PriorityQueue<int[]> newPriorityQueue() {
+        return new PriorityQueue<>((t1, t2) -> {
+            // 위쪽 우선 i 가 작은게 앞으로
+            // 왼쪽 우선 j가 작은게 앞으로
+            if (t1[0] != t2[0]) return t1[0] - t2[0];
+            else return t1[1] - t2[1];
+        });
+    }
+
 }
 
 
